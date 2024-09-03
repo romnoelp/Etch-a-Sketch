f219d361842131964937be57e3d784584c05b48e
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-09-03 15:32:47 +0800

    Finished the project
    Got some inspiration for the designs of the grids and the buttons from the net, and I must say, hafta knack my brain for this one.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gridContainer = document.querySelector(".grid-container");
-let gridCount = 16;
+let gridCount = 32;
 
 // Rendering the grid container
 const gridRenderer = (newGridCount) => {
@@ -15,13 +15,12 @@ gridRenderer(gridCount);
 // Logic for changing the number of grids
 const changeValueButton = document.querySelector(".header .change-grid");
 
-changeValueButton.addEventListener("click", function (element) {
+changeValueButton.addEventListener("click", () => {
   newGridCount = prompt("Give me the new value for the grid");
   if (newGridCount <= 64) {
     gridRenderer(newGridCount);
-  }
-  else  {
-    alert("I'm sorry, can't put more thant 64 grids inside the grid.");
+  } else {
+    alert("I'm sorry, can't put more than 64 grids inside the grid.");
     gridRenderer(gridCount);
   }
 });
